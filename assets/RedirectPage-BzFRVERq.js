8864dc5b4124adde76c39af838eb771e5d81bfe3
Harry Mahardhika <harrymahardhika@gmail.com>
2025-07-19 14:20:15 +0700

    update

--- assets/RedirectPage-BzFRVERq.js
+++
@@ -1,1 +0,0 @@
-import{d as n,u as r,s as u,p as c,i,c as m,a as p}from"./index-ChONLvXT.js";import{u as d}from"./title-B0mAHwLg.js";const R=n({__name:"RedirectPage",setup(h){d("Redirecting...");const e=i(),t=r(),{user:s,isAuthenticated:o}=u(t);c(()=>{a()});const a=()=>{s.value&&o?e.push({name:"home"}):e.push({name:"auth.login"})};return(l,_)=>(p(),m("div"))}});export{R as default};
--- assets/title-B0mAHwLg.js
+++
@@ -1,1 +0,0 @@
-import{g as r,ah as a}from"./index-ChONLvXT.js";function u(i){const t=r(i);return a(t,e=>{e&&(document.title=e)},{immediate:!0}),t}export{u};
